1534981509fcc037bbc54f74318191845de94e63
David Nguyen <d.nguyen@wolt.com>
2024-02-16 22:26:28 +0700

    first commit

--- src/pages/api/hello.ts
+++ src/pages/api/hello.ts
@@ -1,13 +1,19 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from "next";
+const data = require("./data.json");
 
 type Data = {
-  name: string;
+  targetUrl: string;
+  imageUrl: string;
+  description: string;
+  startDate: string;
+  endDate: string;
+  isActive: boolean;
 };
 
 export default function handler(
   req: NextApiRequest,
-  res: NextApiResponse<Data>,
+  res: NextApiResponse<Data>
 ) {
-  res.status(200).json({ name: "John Doe" });
+  res.status(200).json(data);
 }
